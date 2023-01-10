786a6c34aecdf67bdd02281df3e142a342a649ea
pedrotanak <106190056+pedrotanak@users.noreply.github.com>
2023-01-10 17:02:58 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -17,3 +17,43 @@ classificador=ml5.imageClassifier("https://teachablemachine.withgoogle.com/model
 function carregou(){
     console.log("Modelo Carregado")
 }
+function falar() {
+    sintetizador = window.speechSynthesis;
+    fala1 = "A primeira emoção é " + emoção1;
+    fala2 = "A segunda emoção é " + emoção2;
+    var voz = new SpeechSynthesisUtterance(fala1 + fala2);
+    sintetizador.speak(voz);
+}
+function vereficar() {
+    imagem = document.getElementById("imagem");
+    classificador.classify(imagem, resposta);
+}
+function resposta(erro, resultados) {
+    if (erro) {
+        console.error(erro);
+    } else {
+        document.getElementById("nomeEmoção").innerHTML = resultados[0].label;
+        document.getElementById("nomeEmoção2").innerHTML = resultados[1].label;
+        emoção1 = resultados[0].label;
+        emoção2 = resultados[1].label;
+        falar()
+        if (emoção1 == "Feliz") {
+            document.getElementById("emogiEmoção").innerHTML = "&#128522;";
+        }
+        if (emoção1 == "Triste") {
+            document.getElementById("emogiEmoção").innerHTML = "&#128532;";
+        }
+        if (emoção1 == "Irritado") {
+            document.getElementById("emogiEmoção").innerHTML = "&#128548;";
+        }
+        if (emoção2 == "Feliz") {
+            document.getElementById("emogiEmoção2").innerHTML = "&#128522;";
+        }
+        if (emoção2 == "Triste") {
+            document.getElementById("emogiEmoção2").innerHTML = "&#128532;";
+        }
+        if (emoção2 == "Irritado") {
+            document.getElementById("emogiEmoção2").innerHTML = "&#128548;";
+        }
+    }
+}
